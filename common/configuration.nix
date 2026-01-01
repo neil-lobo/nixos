e8923b47c9517622b0fa6dbf4bfcb88be2388796
neil-lobo <neil_edward_lobo@hotmail.com>
2025-12-31 22:31:31 -0500

    chore: remove old nvidia comments

--- common/configuration.nix
+++ common/configuration.nix
@@ -137,21 +137,6 @@ in
       };
       open = true;
       nvidiaSettings = true;
-      #      package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-      #        version = "575.51.02";
-      #        sha256_64bit = "sha256-XZ0N8ISmoAC8p28DrGHk/YN1rJsInJ2dZNL8O+Tuaa0=";
-      #        sha256_aarch64 = "sha256-NNeQU9sPfH1sq3d5RUq1MWT6+7mTo1SpVfzabYSVMVI=";
-      #        openSha256 = "sha256-NQg+QDm9Gt+5bapbUO96UFsPnz1hG1dtEwT/g/vKHkw=";
-      #        settingsSha256 = "sha256-6n9mVkEL39wJj5FB1HBml7TTJhNAhS/j5hqpNGFQE4w=";
-      #        persistencedSha256 = "sha256-dgmco+clEIY8bedxHC4wp+fH5JavTzyI1BI8BxoeJJI=";
-      #      };
-
-      #       package = config.boot.kernelPackages.nvidiaPackages.beta.overrideAttrs {
-      #         src = pkgs.fetchurl {
-      #           url = "https://download.nvidia.com/XFree86/Linux-x86_64/575.51.02/NVIDIA-Linux-x86_64-575.51.02.run";
-      #           sha256 = "sha256-XZ0N8ISmoAC8p28DrGHk/YN1rJsInJ2dZNL8O+Tuaa0=";
-      #         };
-      #       };
       package = config.boot.kernelPackages.nvidiaPackages.stable;
     };
   };
@@ -231,10 +216,6 @@ in
     variables = {
       CHATTERINO2_RECENT_MESSAGES_URL = "https://logs.zonian.dev/rm/%1";
       EDITOR = "vim";
-      #       __NV_PRIME_RENDER_OFFLOAD="1";
-      #       __NV_PRIME_RENDER_OFFLOAD_PROVIDER="NVIDIA-G0";
-      #       __GLX_VENDOR_LIBRARY_NAME="nvidia";
-      #       __VK_LAYER_NV_optimus="NVIDIA_only";
     };
   };
 }
